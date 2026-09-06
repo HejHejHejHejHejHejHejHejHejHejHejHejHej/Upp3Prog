009ea74a309eee1cea3cc9b1ed52a4e5e518bcb6
Samuel Moberg <samuel.moberg@hotmail.se>
2026-09-06 19:34:51 +0200

    Modul 3

---
+++ Upp3.py
@@ -0,0 +1,46 @@
+while True: 
+    try:
+        Hight = float(input("Hur Inte kortt är du i centimeter: "))
+        break
+    except:
+        print("Bara gör rätt nu okej")
+        continue
+
+while True: 
+    try:
+        Weight = float(input("Hur mycket väger du i Kilogram: "))
+        break
+    except:
+        print("Bara gör rätt nu okej")
+        continue
+
+while True: 
+    try:
+        r = int(input("radien på en cirkel: "))
+        break
+    except:
+        print("Bara gör rätt nu okej")
+        continue
+
+while True: 
+    try:
+        AT = int(input("Antal tärningar: "))
+        break
+    except:
+        print("Bara gör rätt nu okej")
+        continue
+
+if Hight >= int(160):
+    print("Du är tillräkligt long")
+else:
+    print("Du är inte tillräkligt long")
+
+print("BMI:", Weight/(Hight/100)**2)
+
+
+print("Cirkelns aria:", r**2*3.14)
+
+import random
+
+for i in range(0, AT):
+    print(random.randint(1, 6))
